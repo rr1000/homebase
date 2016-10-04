0cb281b25082057386fe0e21dfcad9f2c55541d8
Ryan Rich <rrichrs@gmail.com>
2016-10-03 20:18:40 -0700

    Trying to fix migrations

--- db/migrate/20161003204643_devise_create_users.rb
+++ db/migrate/20161003204643_devise_create_users.rb
@@ -30,21 +30,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      # Invitable
-      t.string   :invitation_token
-      t.datetime :invitation_created_at
-      t.datetime :invitation_sent_at
-      t.datetime :invitation_accepted_at
-      t.integer  :invitation_limit
-      t.integer  :invited_by_id
-      t.string   :invited_by_type
-
       t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    add_index :users, :invitation_token, :unique => true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
