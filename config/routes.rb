7a4a8d9d15629a5ca8445136beef6f3ab0d4b04f
Ryan Rich <rrichrs@gmail.com>
2016-10-05 11:23:41 -0700

    Adding schedule for day one

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
     devise_scope :user do
         get '/logout', to: 'devise/sessions#destroy'
         get '/sign-in', to: 'devise/sessions#new'
-        get '/register0912e891282999asd112', to: 'devise_invitable/registrations#new'
     end
     get '/no-access', to: 'no_access#show'
     root 'static_pages#schedule'
