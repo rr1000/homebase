83eecb90d7ada590afe78e47b554a5bfd42c456e
Ryan Rich <rrichrs@gmail.com>
2016-10-05 11:32:21 -0700

    Opening registration for like a second

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,8 @@
 class RegistrationsController < Devise::RegistrationsController
-    def new
-        redirect_to no_access_path
-    end
-    def create
-        redirect_to no_access_path
-    end
+    # def new
+    #     redirect_to no_access_path
+    # end
+    # def create
+    #     redirect_to no_access_path
+    # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-    devise_for :users, :controllers => { :registrations => 'registrations' }
+    # , :controllers => { :registrations => 'registrations' }
+    devise_for :users
     devise_scope :user do
         get '/logout', to: 'devise/sessions#destroy'
         get '/sign-in', to: 'devise/sessions#new'
     end
-    get '/no-access', to: 'no_access#show'
+    # get '/no-access', to: 'no_access#show'
     root 'static_pages#schedule'
 end
