4d18c082c5e51facc49c94ddba6673bcc55f7461
Ryan Rich <rrichrs@gmail.com>
2016-10-05 11:34:44 -0700

    Closing registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-    # , :controllers => { :registrations => 'registrations' }
-    devise_for :users
+    devise_for :users, :controllers => { :registrations => 'registrations' }
     devise_scope :user do
         get '/logout', to: 'devise/sessions#destroy'
         get '/sign-in', to: 'devise/sessions#new'
     end
-    # get '/no-access', to: 'no_access#show'
+    get '/no-access', to: 'no_access#show'
     root 'static_pages#schedule'
 end
