6d5723ddb9a78bf1d3c3dd7e1de36875e159e429
Ryan Rich <rrichrs@gmail.com>
2016-10-07 15:40:44 -0700

    Bad

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,3 @@
 class RegistrationsController < Devise::RegistrationsController
-    def new
-        redirect_to no_access_path
-    end
-    def create
-        redirect_to no_access_path
-    end
+
 end
