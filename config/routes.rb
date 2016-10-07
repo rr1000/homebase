d8d18fad4b0f773404a2fc7361aa2df24d6cc15a
Ryan Rich <rrichrs@gmail.com>
2016-10-07 16:17:44 -0700

    closed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-    devise_for :users
+    devise_for :users, :controllers => { :registrations => 'registrations' }
     devise_scope :user do
         get '/logout', to: 'devise/sessions#destroy'
         get '/sign-in', to: 'devise/sessions#new'
