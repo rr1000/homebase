290a6366de0cb0f4ef947575c4d8d69f891007c6
Ryan Rich <rrichrs@gmail.com>
2016-10-12 17:19:51 -0500

    Adding mail and fixing something

--- config/environment.rb
+++ config/environment.rb
@@ -3,16 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ActionMailer::Base.default_options = { from: "ryan@catalyze.io" }
-ActionMailer::Base.default :charset => "utf-8"
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-    user_name: ENV["USERNAME"],
-    password: ENV["PASSWORD"],
-    domain: 'catalyze.io',
-    address: 'smtp.mailgun.org',
-    port: 587,
-    authentication: :login,
-    enable_starttls_auto: true
-}
